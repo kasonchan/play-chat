ff7addc3ec83b330bf13162430226e335bbe6722
Kason <kasonl.chan@gmail.com>
2015-05-17 23:45:27 -0500

    Add favicon_white; update stylesheets

--- app/assets/javascripts/chat.js
+++ app/assets/javascripts/chat.js
@@ -3,7 +3,26 @@
  */
 
 $(document).ready(function () {
-
   // Hide error message
   $("#div-error").hide();
 })
+
+/**
+ * Sticky relocate
+ * Add stick class when it set off the sticky-anchor
+ * Otherwise remove stick class
+ */
+function sticky_relocate() {
+  var window_top = $(window).scrollTop();
+  var div_top = $('#sticky-anchor').offset().top;
+  if (window_top > div_top) {
+    $('#div-msgs-input').addClass('stick');
+  } else {
+    $('#div-msgs-input').removeClass('stick');
+  }
+}
+
+$(function () {
+  $(window).scroll(sticky_relocate);
+  sticky_relocate();
+});
